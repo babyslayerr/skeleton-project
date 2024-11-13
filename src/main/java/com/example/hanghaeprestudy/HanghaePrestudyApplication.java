59b269e9020f86011bc5d05daa5444b714449047
정종환 <gksqlcxkdns1@naver.com>
2024-11-13 17:41:24 +0900

    firstCommit

---
+++ src/main/java/com/example/hanghaeprestudy/HanghaePrestudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.hanghaeprestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaePrestudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanghaePrestudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hanghaeprestudy/HanghaePrestudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hanghaeprestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanghaePrestudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
