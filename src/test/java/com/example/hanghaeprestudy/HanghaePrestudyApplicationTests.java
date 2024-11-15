3e7160a35a95b86d5c93e3cf16930f82f40fd583
정종환 <gksqlcxkdns1@naver.com>
2024-11-15 22:23:56 +0900

    test코드 추가

--- src/test/java/com/example/hanghaeprestudy/HanghaePrestudyApplicationTests.java
+++ src/test/java/com/example/hanghaeprestudy/HanghaePrestudyApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.hanghaeprestudy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,13 @@ class HanghaePrestudyApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@DisplayName("전체 게시글 목록 조회")
+	@Test
+	void 전체게시글목록조회(){
+//		List<Post> posts = postService.findAll();
+//		Assertions.assertNotEquals(0,posts.size());
 	}
 
 }
