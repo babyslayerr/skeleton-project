60da4877b436eab27dd6bf9ce7521250f5397270
정종환 <gksqlcxkdns1@naver.com>
2024-11-25 13:51:31 +0900

    usecase, erd 수정

--- src/test/java/com/example/hanghaeprestudy/HanghaePrestudyApplicationTests.java
+++ src/test/java/com/example/hanghaeprestudy/HanghaePrestudyApplicationTests.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootTest
+@EnableJpaAuditing
 class HanghaePrestudyApplicationTests {
 
 	@Test
