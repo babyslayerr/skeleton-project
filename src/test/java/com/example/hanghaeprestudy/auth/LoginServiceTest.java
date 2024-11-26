0b0954fd574e4fcd9fa55fcca1b80c463bfc11f3
정종환 <gksqlcxkdns1@naver.com>
2024-11-26 15:52:37 +0900

    JWT 의존성 추가 및 테스트

--- src/test/java/com/example/hanghaeprestudy/auth/LoginServiceTest.java
+++ src/test/java/com/example/hanghaeprestudy/auth/LoginServiceTest.java
@@ -1,8 +1,19 @@
 package com.example.hanghaeprestudy.auth;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.security.*;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.RSAPrivateKeySpec;
 
 public class LoginServiceTest {
 
@@ -26,5 +37,50 @@ public class LoginServiceTest {
 
         // then
         Assertions.assertNotNull(postLoginResponse);
+    }
+
+    @Test
+    void 토큰생성테스트(){
+
+        PublicKey publicKey= null;
+        PrivateKey privateKey = null;
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+
+            keyPairGenerator.initialize(2048);
+
+            KeyPair keyPair = keyPairGenerator.genKeyPair();
+            publicKey = keyPair.getPublic();
+            privateKey = keyPair.getPrivate();
+
+            Algorithm algorithm = Algorithm.RSA256((RSAPublicKey) publicKey, (RSAPrivateKey) privateKey);
+            String token = JWT.create()
+                    .withClaim("key","value")
+                    .withIssuer("auth0")
+                    .sign(algorithm);
+
+            DecodedJWT decodedJWT;
+
+            JWTVerifier verifier = JWT.require(algorithm)
+                    // specify any specific claim validations
+                    .withIssuer("auth0")
+                    // reusable verifier instance
+                    .build();
+
+            decodedJWT = verifier.verify(token);
+
+
+            System.out.println(decodedJWT.getClaim("key"));
+
+        }
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }catch (JWTCreationException exception){
+            // Invalid Signing configuration / Couldn't convert Claims.
+        }
+        catch (JWTVerificationException exception){
+            // Invalid signature/claims
+        }
+
     }
 }
